46e42b318c1093d72f07dc0179590cf683b6d0db
victsomie <victsomie@yahoo.com>
2016-05-28 07:38:48 +0300

    modified more text

--- TextViewApp/app/src/main/java/com/example/victor/textviewapp/MainActivity.java
+++ TextViewApp/app/src/main/java/com/example/victor/textviewapp/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
 
         ListView listView = (ListView) findViewById(R.id.mobile_list);
