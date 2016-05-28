d95267c7e992e3b6c73f8d6c93d9c6543e7e133b
victsomie <victsomie@yahoo.com>
2016-05-28 07:19:21 +0300

    Added text view project

--- TextViewApp/app/src/main/java/com/example/victor/textviewapp/MainActivity.java
+++ TextViewApp/app/src/main/java/com/example/victor/textviewapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.victor.textviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Have an array which you want to display
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+
     }
 }
